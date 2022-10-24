68b51d7eed4cec5871675c164861a0b5e5463dd7
Matthew Faigan <matthew.faigan@gmail.com>
2022-10-24 12:11:41 -0400

    Create plane class

---
+++ ATC/src/Plane.cpp
@@ -0,0 +1,9 @@
+/*
+ * Plane.cpp
+ *
+ *  Created on: Oct. 24, 2022
+ *      Author: matt
+ */
+
+#include "Plane.h"
+
---
+++ ATC/src/Plane.h
@@ -0,0 +1,14 @@
+/*
+ * Plane.h
+ *
+ *  Created on: Oct. 24, 2022
+ *      Author: Matthew Faigan
+ */
+
+#ifndef SRC_PLANE_H_
+#define SRC_PLANE_H_
+
+class Plane {
+};
+
+#endif /* SRC_PLANE_H_ */
