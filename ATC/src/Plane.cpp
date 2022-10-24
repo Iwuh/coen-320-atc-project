5e241d9b6f2d73230ad25f866866f5f32ea41fe4
Matthew Faigan <matthew.faigan@gmail.com>
2022-10-24 13:16:06 -0400

    Add thread start functionality to plane

--- ATC/src/Plane.cpp
+++ ATC/src/Plane.cpp
@@ -2,8 +2,27 @@
  * Plane.cpp
  *
  *  Created on: Oct. 24, 2022
- *      Author: matt
+ *      Author: Matthew Faigan
  */
 
 #include "Plane.h"
 
+Plane::Plane(PlaneStartParams &params)
+	: startParams(params)
+{}
+
+void Plane::Run()
+{
+}
+
+Plane* Plane::CreateWithParams(PlaneStartParams &params)
+{
+	return new Plane(params);
+}
+
+void* Plane::Start(void *context)
+{
+	auto p = (Plane*)context;
+	p->Run();
+	return NULL;
+}
--- ATC/src/Plane.h
+++ ATC/src/Plane.h
@@ -8,7 +8,38 @@
 #ifndef SRC_PLANE_H_
 #define SRC_PLANE_H_
 
+#include <stdlib.h>
+
+typedef struct
+{
+	int x;
+	int y;
+	int z;
+} Vec3;
+
+typedef struct
+{
+	int id;
+	int arrivalTime;
+	Vec3 initialPosition;
+	Vec3 initialVelocity;
+} PlaneStartParams;
+
 class Plane {
+
+private:
+	Plane(PlaneStartParams& params);
+
+	void Run();
+
+	PlaneStartParams startParams;
+
+public:
+	// Create a new instance of the Plane class with the specified parameters.
+	static Plane* CreateWithParams(PlaneStartParams& params);
+
+	// Thread host function to initialize the plane. Use as target for pthread_create.
+	static void* Start(void* context);
 };
 
 #endif /* SRC_PLANE_H_ */
