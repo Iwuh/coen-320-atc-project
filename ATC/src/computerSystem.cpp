94d76fe08f57949850fa99cfdcd0c1ae235a6a3e
aiceball <abikalapov+git@gmail.com>
2022-11-15 12:24:38 -0500

    Playing around with how to use maps for space representation

---
+++ ATC/src/computerSystem.cpp
@@ -0,0 +1,84 @@
+/* class ComputerSystem
+ * int ID
+ * list of aircraft in the airspace: position - x y z
+ *
+ * function violationScan - periodic
+ * function alert - critical sporadic
+ * sendAircraftInfo - id, position - periodic
+ * processOperatorRequests - aperiodic
+ * logState - periodic
+ */
+#include <map>
+#include <string>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+struct Position {
+	Position() : x_coord(0), y_coord(0), z_coord(0) {}
+	Position(int x_coord, int y_coord, int z_coord) : x_coord(x_coord), y_coord(y_coord), z_coord(z_coord) {}
+	int x_coord;
+	int y_coord;
+	int z_coord;
+	const int Y_CAP=3000;
+	const int X_CAP=3000;
+	const int Z_CAP=1000;
+};
+
+class ComputerSystem
+{
+private:
+	std::map<int, Position> airspace;
+	void violationScan(int n){
+
+	}
+	void alert(){
+
+	}
+	void sendAircraftInfo(){
+
+	}
+	void processOperatorRequest(){
+
+	}
+	void printCurrentTime(){
+		char s[1000];
+
+		time_t t = time(NULL);
+		struct tm * p = localtime(&t);
+
+		strftime(s, 1000, "%A, %B %d %Y", p);
+
+		printf("%s ", s);
+	}
+public:
+	ComputerSystem()
+	{
+	}
+	static void* Start(void* context);
+	void logState(){
+		printCurrentTime();
+		for (auto const& x : airspace){
+			printf(" %d:%s,%s,%s |", x.first,std::to_string(x.second.x_coord),std::to_string(x.second.y_coord),std::to_string(x.second.y_coord));
+//		    std::cout << x.first  // string (key)
+//		              << ':'
+//		              << x.second.x_coord // string's value
+//		              << x.second.y_coord // string's value
+//		              << x.second.z_coord // string's value
+//		              << std::endl;
+		}
+	}
+	void updateAirspace(int ID, Position pos){
+		airspace.insert(pair<int,Position>(ID,pos));
+	}
+};
+
+int main(int argc, char **argv) {
+	ComputerSystem c = ComputerSystem();
+	Position pos = Position(1,2,3);
+	for (int i = 1; i< 10; i++){
+		c.updateAirspace(i,pos);
+	}
+	c.logState();
+}
