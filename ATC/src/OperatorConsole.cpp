21f80b4bef2767a2ab48792bebc6167541d2b67d
Matthew Faigan <matthew.faigan@gmail.com>
2022-12-02 16:18:14 -0500

    Partial work on operator console

--- ATC/src/OperatorConsole.cpp
+++ ATC/src/OperatorConsole.cpp
@@ -7,9 +7,69 @@
 
 #include "OperatorConsole.h"
 
+OperatorConsole::OperatorConsole()
+	: chid(-1)
+{}
+
 void OperatorConsole::run()
 {
+	// Create a communication channel
+	if ((chid = ChannelCreate(0)) == -1)
+	{
+		std::cout << "Operator console: channel creation failed. Exiting thread." << std::endl;
+		return;
+	}
 
+	// Open a client to our own connection to be used for timer pulses and store the handle in coid.
+	int coid;
+	if ((coid = ConnectAttach(0,0,chid,0,0)) == -1)
+	{
+		std::cout << "Operator console: failed to attach to self. Exiting thread.";
+		return;
+	}
+
+	// Initialize a sigevent to send a pulse.
+	struct sigevent sigev;
+	SIGEV_PULSE_INIT(&sigev, coid, SIGEV_PULSE_PRIO_INHERIT, OPCON_CODE_TIMER, 0);
+
+	timer_t updateTimer;
+	if (timer_create(CLOCK_MONOTONIC, &sigev, &updateTimer) == -1)
+	{
+		std::cout << "Operator console: failed to initialize update timer. Exiting thread.";
+		return;
+	}
+
+	// Set the timer to check for new user commands every second.
+	struct itimerspec timerValue;
+	timerValue.it_value.tv_sec = 1;
+	timerValue.it_value.tv_nsec = 0;
+	timerValue.it_interval.tv_sec = 1;
+	timerValue.it_interval.tv_nsec = 0;
+
+	// Start the timer.
+	timer_settime(updateTimer, 0, &timerValue, NULL);
+
+	// Start listening for messages
+	listen();
+}
+
+void OperatorConsole::listen()
+{
+	int rcvid;
+	OperatorConsoleCommandMessage msg;
+	while (1)
+	{
+		rcvid = MsgReceive(chid, &msg, sizeof(msg), NULL);
+		if (rcvid == 0)
+		{
+			// Received a pulse
+			switch (msg.header.code)
+			{
+			case OPCON_CODE_TIMER:
+
+			}
+		}
+	}
 }
 
 void* OperatorConsole::start(void* context)
--- ATC/src/OperatorConsole.h
+++ ATC/src/OperatorConsole.h
@@ -8,9 +8,43 @@
 #ifndef SRC_OPERATORCONSOLE_H_
 #define SRC_OPERATORCONSOLE_H_
 
+#include <stdlib.h>
+#include <sys/neutrino.h>
+#include "Plane.h"
+
+#define OPCON_CONSOLE_COMMAND_GET_USER_COMMAND 1
+#define OPCON_CONSOLE_COMMAND_EXIT_THREAD 2
+
+#define OPCON_USER_COMMAND_NO_COMMAND_AVAILABLE 1
+#define OPCON_USER_COMMAND_DISPLAY_PLANE_INFO 2
+#define OPCON_USER_COMMAND_SET_PLANE_VELOCITY 3
+
+#define OPCON_CODE_TIMER 1
+
+typedef struct
+{
+	struct _pulse header;
+	int query;
+} OperatorConsoleCommandMessage;
+
+typedef struct
+{
+	int userCommandType;
+	union commandParams {
+		int planeNumber;
+		Vec3 newVelocity;
+	};
+} OperatorConsoleResponseMessage;
+
 class OperatorConsole {
+public:
+	int getChid() const;
+
 private:
 	void run();
+	void listen();
+
+	int chid;
 
 public:
 	static void* start(void* context);
