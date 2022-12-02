2ee4cbf0c781dcf74e0466f1710074af2c21ca21
Ben Hitterman <71104449+benhitterman@users.noreply.github.com>
2022-12-01 19:12:42 -0500

    Adding Grid Visual Representation

--- ATC/src/DataDisplay.cpp
+++ ATC/src/DataDisplay.cpp
@@ -52,7 +52,7 @@ void DataDisplay::receiveMessage() {
 			MsgReply(rcvid, EOK, NULL, 0);
 			for (int i = 0; i < msg.CommandBody.multiple.numberOfAircrafts; i++) {
 			//std::cout <<"Aircraft positions: " <<msg.CommandBody.multiple->positionArray <<"  " <<"Aircraft velocities: " <<msg.commandBody.multiple->velocityArray <<std::endl;
-				std::cout <<"Aircraft " <<i + 1 <<" with position: " <<msg.CommandBody.multiple->positionArray[i] <<" and velocity: " <<msg.CommandBody.multiple->velocity[i] <<std:endl;
+				std::cout <<"Aircraft " <<i + 1 <<" with position: " <<msg.CommandBody.multiple.positionArray[i] <<" and velocity: " <<msg.CommandBody.multiple.velocity[i] <<std:endl;
 			}
 			//TODO: print data for multiple aircrafts
 			break;
@@ -61,6 +61,39 @@ void DataDisplay::receiveMessage() {
 		{
 			MsgReply(rcvid, EOK, NULL, 0);
 			std::cout <<"Airspace separation constraint of aircraft with ID: " <<msg.commandType.one.aircraftID <<" and position: " <<msg.commandType.one.position <<std::endl;
+			break;
+		}
+		case COMMAND_GRID: //ignoring z-axis, doing x and y (top-view)
+		{
+			MsgReply(rcvid, EOK, NULL, 0);
+
+			int rowSize = 5;
+			int columnSize = 10;
+			int cellSize = 20;
+
+			string grid[rowSize][columnSize]; //grid 100m x 200m with each square being 20m
+			//storing into grid
+			for (int i = 0; i < msg.commandBody.multiple.numberOfAircrafts; i++) {
+				for (int j = 0; j < rowSize; j++) {
+					if (msg.commandBody.mutiple.positionArray[i].y >= (cellSize * j)  && msg.commandBody.mutiple.positionArray[i].y < (cellSize * (j + 1))) { //checking y
+						for (int k = 0; k < columnSize; k++) {
+							//only for x
+							if (msg.commandBody.mutiple.positionArray[i].x >= (cellSize * k)  && msg.commandBody.mutiple.positionArray[i].x < (cellSize * (k + 1))) { // if plane in row i is between 0 and 20 not included, add to string
+								grid[j][k] += msg.commandBody.mutiple.planeIDArray[i] + ",";
+							}
+						}
+					}
+				}
+			}
+
+			for (int i = 0; i < rowSize; i++) {
+				cout <<std::endl;
+				for (int j = 0; j < columnSize; j++) {
+					cout <<"| " + grid[i][j] + " ";
+				}
+			}
+
+			//print grid
 			break;
 		}
 		}
